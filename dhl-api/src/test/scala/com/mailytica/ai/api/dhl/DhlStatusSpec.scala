938f6502fe8524c927a264396462706ddba90263
msouvikrepo <souvik.mondal@uni.kn>
2023-04-03 09:00:16 +0200

    Scala test

---
+++ dhl-api/src/test/scala/com/mailytica/ai/api/dhl/DhlStatusSpec.scala
@@ -0,0 +1,10 @@
+package com.mailytica.ai.api.dhl
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import spray.json.{JsValue, JsonParser}
+import java.time.{ZoneId, ZonedDateTime}
+
+class DhlStatusSpec {
+
+}
