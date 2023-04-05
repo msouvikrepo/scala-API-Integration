5527f4ca1c35a1f050d2775ca09c747622b9648d
msouvikrepo <souvik.mondal@uni.kn>
2023-04-05 15:52:52 +0200

    test palceholders removed

--- dhl-api/src/test/scala/com/mailytica/ai/api/freshdesk/FreshdeskSpec.scala
+++ dhl-api/src/test/scala/com/mailytica/ai/api/freshdesk/FreshdeskSpec.scala
@@ -16,9 +16,10 @@ class FreshdeskSpec extends FlatSpec with Matchers with OptionValues {
 
   it should "convert a json of an empty freshdesk to_email and return a None" in {
     val freshdeskWithoutEmail = Freshdesk(
-      bodyText = BodyText(""),
-      id = Id(???),
-      supportEmail = SupportEmail(Email("")),
+      bodyText = BodyText("Hi Lea Artner, Antwort1 auf Testticket"),
+      id = Id(101002738565L),
+      supportEmail =
+        SupportEmail(Email("support@newaccount1636718700987.freshdesk.com")),
       toEmails = ToEmails(Seq.empty)
     )
     val (jsonInput, _) = FreshdeskSpec.defaultJsonFixture(freshdeskWithoutEmail)
@@ -33,21 +34,50 @@ object FreshdeskSpec {
 
   def defaultJsonFixture(
                           freshdesk: Freshdesk = Freshdesk(
-                            bodyText = BodyText(""),
-                            id = Id(???),
-                            supportEmail = SupportEmail(???),
-                            toEmails = ToEmails(Seq())
+                            bodyText = BodyText("Hi Lea Artner, Antwort1 auf Testticket"),
+                            id = Id(101002738565L),
+                            supportEmail =
+                              SupportEmail(Email("support@newaccount1636718700987.freshdesk.com")),
+                            toEmails = ToEmails(Seq(Email("lea.artner@gmx.de")))
                           )
                         ): (JsValue, Freshdesk) = {
+    val freshdeskJson =
+      """
+        |{
+        |  "body_text": "Hi Lea Artner, Antwort1 auf Testticket",
+        |  "id": 101002738565,
+        |  "incoming": false,
+        |  "private": false,
+        |  "user_id": 101002862811,
+        |  "support_email": "support@newaccount1636718700987.freshdesk.com",
+        |  "source": 0,
+        |  "category": 1,
+        |  "to_emails": [
+        |    "lea.artner@gmx.de"
+        |  ],
+        |  "from_email": "Mailytica <support@newaccount1636718700987.freshdesk.com>",
+        |  "cc_emails": [
+        |  ],
+        |  "bcc_emails": [
+        |  ],
+        |  "email_failure_count": null,
+        |  "outgoing_failures": null,
+        |  "thread_id": null,
+        |  "thread_message_id": null,
+        |  "created_at": "2021-11-19T12:20:19Z",
+        |  "updated_at": "2021-11-19T12:20:19Z",
+        |  "last_edited_at": null,
+        |  "last_edited_user_id": null,
+        |  "attachments": [
+        |  ],
+        |  "automation_id": null,
+        |  "automation_type_id": null,
+        |  "auto_response": false,
+        |  "ticket_id": 16,
+        |  "source_additional_info": null
+        |}
+        |""".stripMargin
 
-    (
-      JsonParser(""),
-      Freshdesk(
-        bodyText = BodyText(""),
-        id = Id(???),
-        supportEmail = SupportEmail(???),
-        toEmails = ToEmails(Seq())
-      )
-    )
+    (JsonParser(freshdeskJson), freshdesk)
   }
 }
