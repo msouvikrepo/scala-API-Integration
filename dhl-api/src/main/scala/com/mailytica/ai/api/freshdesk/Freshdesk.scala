424e765c7b15dc5305184efbceebdb399dbfdeca
msouvikrepo <souvik.mondal@uni.kn>
2023-04-05 14:58:25 +0200

    New Freshdesk

--- dhl-api/src/main/scala/com/mailytica/ai/api/freshdesk/Freshdesk.scala
+++ dhl-api/src/main/scala/com/mailytica/ai/api/freshdesk/Freshdesk.scala
@@ -1,4 +1,14 @@
 package com.mailytica.ai.api.freshdesk
 
 case class Freshdesk(
-                    )
+                    ){
+
+}
+
+object Freshdesk{
+
+  def parseJson(jsonString : String): Freshdesk = {
+    ???
+  }
+
+}
--- dhl-api/src/test/scala/com/mailytica/ai/api/freshdesk/FreshdeskSpec.scala
+++ dhl-api/src/test/scala/com/mailytica/ai/api/freshdesk/FreshdeskSpec.scala
@@ -1,5 +1,30 @@
 package com.mailytica.ai.api.freshdesk
 
-class FreshdeskSpec {
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import spray.json.{JsValue, JsonParser}
 
+@RunWith(classOf[JUnitRunner])
+class FreshdeskSpec extends FlatSpec with Matchers with OptionValues {
+
+  it should "convert a json of a freshdesk response to a freshdesk object" in{
+
+
+  }
+
+  it should "convert a json of an empty freshdesk to_email and return a None" in{
+
+
+  }
+
+}
+
+object FreshdeskSpec{
+
+  def defaultJsonFixture(
+
+                        ) : Freshdesk = {
+
+  Freshdesk()}
 }
