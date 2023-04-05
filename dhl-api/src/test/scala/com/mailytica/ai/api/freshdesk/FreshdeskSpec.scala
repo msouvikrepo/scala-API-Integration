a26c4c119ec0a0d6eb2e4a104bb9ac05d44a5a8b
msouvikrepo <souvik.mondal@uni.kn>
2023-04-05 15:36:18 +0200

    formatted test

--- dhl-api/src/test/scala/com/mailytica/ai/api/freshdesk/FreshdeskSpec.scala
+++ dhl-api/src/test/scala/com/mailytica/ai/api/freshdesk/FreshdeskSpec.scala
@@ -10,17 +10,18 @@ class FreshdeskSpec extends FlatSpec with Matchers with OptionValues {
 
   it should "convert a json of a freshdesk response to a freshdesk object" in {
     val (jsonInput, expectedFreshdesk) = FreshdeskSpec.defaultJsonFixture()
-    val actualFreshdesk : Freshdesk = Freshdesk.parseJson(jsonInput.toString())
+    val actualFreshdesk: Freshdesk = Freshdesk.parseJson(jsonInput.toString())
     expectedFreshdesk shouldEqual actualFreshdesk
   }
 
   it should "convert a json of an empty freshdesk to_email and return a None" in {
-  val freshdeskWithoutEmail = Freshdesk(bodyText = BodyText(""),
-    id = Id(???),
-    supportEmail = SupportEmail(Email("")),
-    toEmails = ToEmails(Seq.empty)
-  )
-    val(jsonInput, _) = FreshdeskSpec.defaultJsonFixture(freshdeskWithoutEmail)
+    val freshdeskWithoutEmail = Freshdesk(
+      bodyText = BodyText(""),
+      id = Id(???),
+      supportEmail = SupportEmail(Email("")),
+      toEmails = ToEmails(Seq.empty)
+    )
+    val (jsonInput, _) = FreshdeskSpec.defaultJsonFixture(freshdeskWithoutEmail)
     val actualFreshdesk = Freshdesk.parseJson(jsonInput.toString())
 
     actualFreshdesk shouldEqual freshdeskWithoutEmail
@@ -30,17 +31,23 @@ class FreshdeskSpec extends FlatSpec with Matchers with OptionValues {
 
 object FreshdeskSpec {
 
-  def defaultJsonFixture(freshdesk: Freshdesk = Freshdesk(
-    bodyText = BodyText(""),
-    id = Id(???),
-    supportEmail = SupportEmail(???),
-    toEmails = ToEmails(Seq())
-  )
+  def defaultJsonFixture(
+                          freshdesk: Freshdesk = Freshdesk(
+                            bodyText = BodyText(""),
+                            id = Id(???),
+                            supportEmail = SupportEmail(???),
+                            toEmails = ToEmails(Seq())
+                          )
                         ): (JsValue, Freshdesk) = {
 
-    (JsonParser(""), Freshdesk(bodyText = BodyText(""),
-      id = Id(???),
-      supportEmail = SupportEmail(???),
-      toEmails = ToEmails(Seq())))
+    (
+      JsonParser(""),
+      Freshdesk(
+        bodyText = BodyText(""),
+        id = Id(???),
+        supportEmail = SupportEmail(???),
+        toEmails = ToEmails(Seq())
+      )
+    )
   }
 }
