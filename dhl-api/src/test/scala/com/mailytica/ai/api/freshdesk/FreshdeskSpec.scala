fe035dfef1d43cdb688056a3538ff9079b8008d7
msouvikrepo <souvik.mondal@uni.kn>
2023-04-05 15:31:09 +0200

    test spec filled in

--- dhl-api/src/test/scala/com/mailytica/ai/api/freshdesk/FreshdeskSpec.scala
+++ dhl-api/src/test/scala/com/mailytica/ai/api/freshdesk/FreshdeskSpec.scala
@@ -3,19 +3,27 @@ package com.mailytica.ai.api.freshdesk
 import org.junit.runner.RunWith
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
-import spray.json.{JsValue, JsonParser}
+import spray.json.{JsValue, JsonParser, jsonReader}
 
 @RunWith(classOf[JUnitRunner])
 class FreshdeskSpec extends FlatSpec with Matchers with OptionValues {
 
   it should "convert a json of a freshdesk response to a freshdesk object" in {
-
-
+    val (jsonInput, expectedFreshdesk) = FreshdeskSpec.defaultJsonFixture()
+    val actualFreshdesk : Freshdesk = Freshdesk.parseJson(jsonInput.toString())
+    expectedFreshdesk shouldEqual actualFreshdesk
   }
 
   it should "convert a json of an empty freshdesk to_email and return a None" in {
+  val freshdeskWithoutEmail = Freshdesk(bodyText = BodyText(""),
+    id = Id(???),
+    supportEmail = SupportEmail(Email("")),
+    toEmails = ToEmails(Seq.empty)
+  )
+    val(jsonInput, _) = FreshdeskSpec.defaultJsonFixture(freshdeskWithoutEmail)
+    val actualFreshdesk = Freshdesk.parseJson(jsonInput.toString())
 
-
+    actualFreshdesk shouldEqual freshdeskWithoutEmail
   }
 
 }
@@ -28,7 +36,7 @@ object FreshdeskSpec {
     supportEmail = SupportEmail(???),
     toEmails = ToEmails(Seq())
   )
-                        ):(JsValue, Freshdesk) = {
+                        ): (JsValue, Freshdesk) = {
 
     (JsonParser(""), Freshdesk(bodyText = BodyText(""),
       id = Id(???),
