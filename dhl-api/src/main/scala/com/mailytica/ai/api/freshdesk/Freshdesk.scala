6404ba957b96354f90f8ec6624793d2b9d5b79e7
msouvikrepo <souvik.mondal@uni.kn>
2023-04-05 16:52:10 +0200

    implemented parse function

--- dhl-api/src/main/scala/com/mailytica/ai/api/freshdesk/Freshdesk.scala
+++ dhl-api/src/main/scala/com/mailytica/ai/api/freshdesk/Freshdesk.scala
@@ -1,4 +1,5 @@
 package com.mailytica.ai.api.freshdesk
+import spray.json._
 
 case class Freshdesk(
                     bodyText: BodyText,
@@ -12,7 +13,22 @@ case class Freshdesk(
 object Freshdesk{
 
   def parseJson(jsonString : String): Freshdesk = {
-    ???
+
+    val jsonValue = jsonString.parseJson
+    val JsString(bodyText) = jsonValue.asJsObject.fields("body_text")
+    val JsString(id) = jsonValue.asJsObject.fields("id")
+    val JsString(freshdeskSupportEmailString) = jsonValue.asJsObject.fields("support_email")
+    val supportEmail = Email(freshdeskSupportEmailString)
+    val JsArray(toEmailsArray) = jsonValue.asJsObject.fields("to_emails")
+    val toEmailsSeq: Seq[Email] = toEmailsArray
+      .map { case JsString(toEmail) => Email(toEmail) }
+    Freshdesk(
+      BodyText(bodyText),
+      Id(id.toLong),
+      SupportEmail(supportEmail),
+      ToEmails(toEmailsSeq)
+    )
+
   }
 
 }
