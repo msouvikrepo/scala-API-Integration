bc540386915224859863de96e84c7b2c509aafea
msouvikrepo <souvik.mondal@uni.kn>
2023-04-06 09:16:18 +0200

    test structure

--- dhl-api/src/main/scala/com/mailytica/ai/api/jira/JiraIssues.scala
+++ dhl-api/src/main/scala/com/mailytica/ai/api/jira/JiraIssues.scala
@@ -2,4 +2,11 @@ package com.mailytica.ai.api.jira
 
 class JiraIssues {List[Issue]}
 
+object JiraIssues{
+
+  def parseJson(jsonString : String): JiraIssues = {
+    ???
+  }
+}
+
 
--- dhl-api/src/test/scala/com/mailytica/ai/api/jira/JiraSpec.scala
+++ dhl-api/src/test/scala/com/mailytica/ai/api/jira/JiraSpec.scala
@@ -1,5 +1,38 @@
 package com.mailytica.ai.api.jira
 
-class JiraSpec {
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import spray.json.{JsString, JsValue, JsonParser, jsonReader}
 
+@RunWith(classOf[JUnitRunner])
+class JiraSpec extends FlatSpec with Matchers with OptionValues {
+
+  it should "convert a json of a jira response with an issue array without elements and return None" in{
+
+  }
+  it should "convert a json of a jira response with an issue array with one element" in {
+
+  }
+  it should "convert a json of a jira response with an issue array with two elements" in {
+
+  }
+
+}
+
+object JiraSpec{
+
+  def defaultJsonFixture(
+                        jiraIssues: JiraIssues = JiraIssues(List[Issue(
+                          Id(""),
+                          Key(""),
+                          Self(""),
+                          ProjectId(""),
+                          ProjectKey("")
+                        )])
+                        ): (JsValue, JiraIssues) = {
+
+    ???
+
+  }
 }
