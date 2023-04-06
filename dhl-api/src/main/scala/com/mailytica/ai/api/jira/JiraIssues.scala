4e7d67fce9a5cbc8af402fa7540f8f7fe7492ef6
msouvikrepo <souvik.mondal@uni.kn>
2023-04-06 12:27:33 +0200

    Final commit

--- dhl-api/src/main/scala/com/mailytica/ai/api/jira/JiraIssues.scala
+++ dhl-api/src/main/scala/com/mailytica/ai/api/jira/JiraIssues.scala
@@ -5,9 +5,28 @@ case class JiraIssues (issues : List[Issue])
 
 object JiraIssues{
 
+  def parseIssueObjectFromIssueElement(issueJsonString : JsValue): Issue = {
+
+    val JsString(id) = issueJsonString.asJsObject.fields("id")
+    val JsString(key) = issueJsonString.asJsObject.fields("key")
+    val JsString(self) = issueJsonString.asJsObject.fields("self")
+    val JsString(projectId) = issueJsonString.asJsObject.fields("fields").asJsObject.fields("project").asJsObject.fields("id")
+    val JsString(projectKey) = issueJsonString.asJsObject.fields("fields").asJsObject.fields("project").asJsObject.fields("key")
+
+    Issue(Id(id),
+      Key(key),
+      Self(self),
+      ProjectId(projectId),
+      ProjectKey(projectKey)
+    )
+  }
   def parseJson(jsonString : String): JiraIssues = {
 
-    ???
+    val jsonValue = jsonString.parseJson
+    val JsArray(issuesArray) = jsonValue.asJsObject.fields("issues")
+    val issuesList = issuesArray.map(parseIssueObjectFromIssueElement).toList
+
+    JiraIssues(issuesList)
   }
 }
 
