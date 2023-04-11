8edc25cc5347116c72bb71b8eee36283927a850e
msouvikrepo <souvik.mondal@uni.kn>
2023-04-11 10:25:27 +0200

    Current state completed parse json

--- dhl-api/src/main/scala/com/mailytica/ai/api/spinoco/SpinocoMessage.scala
+++ dhl-api/src/main/scala/com/mailytica/ai/api/spinoco/SpinocoMessage.scala
@@ -1,5 +1,6 @@
 package com.mailytica.ai.api.spinoco
 
+import spray.json.DefaultJsonProtocol.{StringJsonFormat, jsonFormat1, seqFormat}
 import spray.json.{JsString, _}
 
 case class SpinocoMessage(
@@ -17,13 +18,21 @@ object SpinocoMessage {
     val spinocoMessageJsValue = jsonString.parseJson
     val JsString(subjectString) = spinocoMessageJsValue.asJsObject.fields("subject")
     val JsArray(hashTagsArray) = spinocoMessageJsValue.asJsObject.fields("hashTags")
-    val JsString(fromSourceEmailString) = spinocoMessageJsValue.asJsObject.fields("from").asJsObject.fields("email")
+    val JsString(fromSourceEmailString) = spinocoMessageJsValue.asJsObject.fields("from").asJsObject.fields("source").asJsObject.fields("email")
     val JsString(emailMessageBody) = spinocoMessageJsValue.asJsObject.fields("emailMessageBody").asJsObject.fields("body")
     val JsArray(destinationToAccountArray) = spinocoMessageJsValue.asJsObject.fields("destination").asJsObject.fields("To")
     val JsString(destinationToAccountEmail) = destinationToAccountArray.head.asJsObject.fields("source").asJsObject.fields("account")
 
+    val hashtagsSeq: Seq[String] = hashTagsArray.map{case JsString(hashtag) => hashtag.toString}
+
+    /*implicit val hashtagFormat = jsonFormat1(Hashtags)
+    val hashtagsSeq = hashTagsArray.convertTo(Hashtags)*/
+
     SpinocoMessage(subject = Subject(subjectString),
-      hashtags = Hashtags(hashTagsArray.toSeq)
+      hashtags = Hashtags(hashtagsSeq),
+      fromSourceEmail = FromSourceEmail(Email(fromSourceEmailString)),
+      emailMessageBody = EmailMessageBody(emailMessageBody),
+      destinationToAccount = DestinationToAccount(Email(destinationToAccountEmail))
     )
 
   }
